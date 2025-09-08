2d1a1a5924915d4232c59737271b7d13da322233
Robin Potze <robin.potze@ordina.nl>
2025-09-08 20:15:10 +0200

    top nav fix etc

--- src/components/navigation/TopNav/TopNav.jsx
+++ src/components/navigation/TopNav/TopNav.jsx
@@ -5,11 +5,11 @@ export default function TopNav() {
         <div className='top-nav' id='top-nav'>
             <a href='/' className='logo-home-link'><img src="/img/logo/logo64.png" alt='robin potze logo' className='logo-header' id='rp-logo' /></a>
             <div className='nav-links' id='nav-links'>
-                <a href='/work' className='nav-link tech-small' id='nav-link-work'>WORK</a>
-                <p className='nav-link tech-small' id='nav-link-separator'>///</p>
-                <a href='/info' className='nav-link tech-small' id='nav-link-about'>INFO</a>
-                <p className='nav-link tech-small' id='nav-link-separator'>///</p>
-                <a href='/contact' className='nav-link tech-small' id='nav-link-contact'>CNTCT</a>
+                <a href='/work' className='nav-link' id='nav-link-work'>WORK</a>
+                <p className='nav-link-divider tech-small' id='nav-link-separator'>///</p>
+                <a href='/info' className='nav-link' id='nav-link-about'>INFO</a>
+                <p className='nav-link-divider tech-small' id='nav-link-separator'>///</p>
+                <a href='/contact' className='nav-link' id='nav-link-contact'>CNTCT</a>
             </div>
         </div>
     );
--- src/routes/Work/Work.jsx
+++ src/routes/Work/Work.jsx
@@ -1,9 +1,19 @@
-import TopNav from '@navigation/TopNav/TopNav';
-import ScrollDown from '@interaction/ScrollDown/ScrollDown';
 
+import ScrollDown from '@interaction/ScrollDown/ScrollDown';
+import TopNav from '@navigation/TopNav/TopNav';
+
+import { useEffect, useState } from 'react';
 import './Work.css';
 
 export default function Work() {
+    const [lineWidth, setLineWidth] = useState(1000)
+
+    useEffect(() => {
+        const line = document.getElementById("header-line");
+        return () => {
+            setLineWidth(line.getBoundingClientRect().width)
+        }
+    })
 
     const entry = {
         id: 1,
@@ -44,6 +54,12 @@ export default function Work() {
                         <img className='hero-title-decal' id='work-hero-decal' src='/img/icon/PLS.svg' alt='Plus Sign' />
                     </div>
                 </div>
+            </div>
+            <div className='work-section' id='work-section-1'>
+                <h3>Synopsis</h3>
+                <svg width="100%" height="2" viewBox="0 0 100% 20" id='header-line'>
+                    <path d={`M0 1 H${lineWidth}`} stroke="var(--c-LGHT)" strokeWidth="1" strokeDasharray="32 128 128 32 128 32 128 128" fill="none" />
+                </svg>
             </div>
         </div >
     );
