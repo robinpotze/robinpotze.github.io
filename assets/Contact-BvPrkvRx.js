528a802e0ac0af593023d5ffc5992c1bd9e965a9
Robin Potze <robin.potze@ordina.nl>
2025-10-22 09:48:29 +0200

    Updates

---
+++ assets/Contact-BvPrkvRx.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Is_D4heu.js";function e(){return t.jsxs("div",{className:"contact-page",children:[t.jsx("h1",{children:"Contact Page"}),t.jsx("p",{children:"This is the contact page."})]})}export{e as default};
