cc250d8c305893fdc8345b9edf82acccdc9c54cc
Robin Potze <robin-potze@live.nl>
2025-12-04 15:02:02 +0100

    back to browserrouter

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,4 +1,4 @@
-import { createHashRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Home from '@routes/Home/Home.jsx';
 import Work from '@routes/Work/Work.jsx';
@@ -6,7 +6,7 @@ import Info from '@routes/Info/Info.jsx';
 import Contact from '@routes/Contact/Contact.jsx';
 import Entry from '@routes/Entry/Entry.jsx';
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
     {
         element: <App />,
         children: [
